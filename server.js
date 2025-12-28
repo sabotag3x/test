ac28b352eed70d4044d6610eae00e5dfb53f7ff2
Henrique <sabotage.sta@gmail.com>
2025-12-27 21:28:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,9 @@ app.post("/api/films", async (req, res) => {
   }
 });
 
-app.listen(3000, () =>
-  console.log("Servidor rodando em http://localhost:3000")
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
+});
+
