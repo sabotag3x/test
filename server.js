8897c16e6a1b86baf7aaace0124e8efbc878c1b6
Henrique <sabotage.sta@gmail.com>
2025-12-27 23:26:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/imdb/:user/:page", async (req, res) => {
   const { user, page } = req.params;
 
-  const url = `https://www.imdb.com/user/${user}/ratings?sort=date_added,desc&mode=detail&page=${page}`;
+  const url = `https://www.imdb.com/user/${user}/ratings?sort=date_added,desc&page=${page}`;
 
   try {
     const r = await fetch(url, {
@@ -20,13 +20,9 @@ app.get("/imdb/:user/:page", async (req, res) => {
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(html);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao buscar IMDb");
+  } catch (e) {
+    res.status(500).send("Erro IMDb");
   }
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Proxy IMDb ativo na porta", PORT);
-});
+app.listen(PORT, "0.0.0.0");
